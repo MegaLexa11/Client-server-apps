bb00817b685de2c2feb3d5a202159d8d176bf3a3
MegaLexa <kukh99@mail.ru>
2022-01-23 14:00:15 +0500

    Homework_6 done

--- Homework_4/common/utils.py
+++ Homework_4/common/utils.py
@@ -1,7 +1,9 @@
 import json
 from .variables import MAX_PACKAGE_LENGTH, ENCODING
+from custom_decorators import log
 
 
+@log
 def get_message(client):
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
     if isinstance(encoded_response, bytes):
@@ -13,6 +15,7 @@ def get_message(client):
     raise ValueError
 
 
+@log
 def send_message(sock, message):
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
